f429cdc969a55fbb8fbf10a9a127cad6cb68ac73
Stratiev <StanislavStratiev@gmail.com>
2022-11-19 22:51:25 +0100

    Cosmetic change to utils.py.

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import requests
 import pandas as pd
 from datetime import datetime
 
-
 def clean_response(res):
     data = res.text.split('\n')
     data = [d.split(',')[1:] for d in data if d != '']
@@ -11,12 +10,11 @@ def clean_response(res):
     df = pd.DataFrame(data, columns=columns)
     return df
 
-
-def prob_from_elo(elo):
+def prob_from_elo(elo_difference):
     """
     The probability formula is taken from http://www.eloratings.net/about
     """
-    return 1/(10**(-elo/400) + 1)
+    return 1/(10**(-elo_difference/400) + 1)
 
 def get_latest_soccer_data():
     now = datetime.now().strftime("%Y-%m-%d")
